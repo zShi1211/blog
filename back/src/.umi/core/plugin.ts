7ad1c3b11bb269e9aa93a41faf8ed8a8a37d750e
zShi1211 <64517373+zShi1211@users.noreply.github.com>
2020-12-29 18:55:11 +0800

    dsf

--- back/src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from 'C:/Users/12/Desktop/blog/back/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','dva','getInitialState','initialStateConfig','request',],
-});
-
-export { plugin };
--- back/src/.umi/plugin-helmet/exports.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-// @ts-ignore
-export { Helmet } from 'C:/Users/12/Desktop/blog/back/node_modules/react-helmet';
