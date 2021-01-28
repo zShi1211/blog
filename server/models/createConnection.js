6640c311e34cd09100428c6d908c20b60254a0f3
zShi1211 <64517373+zShi1211@users.noreply.github.com>
2021-01-28 10:51:53 +0800

    centos

--- server/models/createConnection.js
+++ server/models/createConnection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.set('useCreateIndex', true);
-mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://192.168.2.2/blog', { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection
 // 建立一个连接
 db.on('open', () => {
